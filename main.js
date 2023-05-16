32f93d47cbcdd77218b7db4841ad261be5d2c117
Ayush Jha <64149497+ayusu@users.noreply.github.com>
2023-05-16 21:35:53 +0530

    New button Added
    Added delete button

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ Array.from(btns).forEach((btn)=>{
             string = ""
             document.querySelector('input').value = string;
         }
+        else if(e.target.innerHTML == "D"){
+            let afterDelete = string.slice(0, -1)          
+            document.querySelector('input').value = afterDelete;
+            string = afterDelete
+
+        }
         else{
             console.log(e.target)
             string = string + e.target.innerHTML;
@@ -19,3 +25,12 @@ Array.from(btns).forEach((btn)=>{
     })
 
 })
+
+
+
+// function deletef(string){
+// let deletebutn = document.querySelector(".clear")
+// deletebutn.addEventListener('click', () =>{
+//     console.log(string.slice(0, -1))
+// })
+// }
